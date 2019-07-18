6e7d08a25f60abdbe9aa37289c425fe506a7d9ef
Saif Shaukat Ansari <saif.ansari@kuliza.com>
2019-07-18 16:13:11 +0530

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import {setTextFilter} from './actions/filters'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description:'Water bill',amount:4550}))
-store.dispatch(addExpense({description:'Gas bill',createdAt:1000}))
-store.dispatch(addExpense({description:'Rent',amount:105000}))
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
